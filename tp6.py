896da65d018e134b1be037be2c9c774620ee7ed1
rivas.julio <juliorivasdev@gmail.com>
2023-10-29 12:29:27 -0300

    tp6

---
+++ tp6.py
@@ -0,0 +1,11 @@
+'''1.	Solicitar al usuario que ingrese números, estos deben guardarse en una lista. Para finalizar el usuario debe ingresar 0, el cual no debe guardarse.'''
+# numbers_list = []
+# while True:
+#     numbers = int(input("Ingresa el numero a guardar: "))
+#     if numbers == 0:
+#         break
+#     numbers_list.append(numbers)
+
+# for number in numbers_list:
+#     print(f"Estos son los numeros que guardaste: {number}")
+    
